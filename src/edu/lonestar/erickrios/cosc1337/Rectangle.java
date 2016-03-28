8b7251411112124302637e527081ec15699e717a
rioser23 <erickrios.my.lonestar.edu>
2016-03-27 21:21:38 -0500

    reformat and add accessors

--- src/edu/lonestar/erickrios/cosc1337/Rectangle.java
+++ src/edu/lonestar/erickrios/cosc1337/Rectangle.java
@@ -8,31 +8,58 @@ package edu.lonestar.erickrios.cosc1337;
  *
  */
 public class Rectangle {
-	/**rectangle width*/
+	/** rectangle width */
 	private double width;
-	/** rectangle length*/
+	/** rectangle length */
 	private double length;
-	
-	/**Constructor
+
+	/**
+	 * Constructor
 	 * 
-	 * @param width-The rectangle's width
-	 * @param length-The rectangle's length
+	 * @param width-The
+	 *            rectangle's width
+	 * @param length-The
+	 *            rectangle's length
 	 */
-	public Rectangle(double width,double length){
-		this.width=width;
-		this.length=length;
+	public Rectangle(double width, double length) {
+		this.width = width;
+		this.length = length;
 	}
-	public double getArea(){
+
+	/**
+	 * Accessor for the width field
+	 * 
+	 * @return width
+	 */
+	public double getWidth() {
+		return width;
+	}
+
+	/**
+	 * Accessor for the length field
+	 * 
+	 * @return length
+	 */
+	public double getLength() {
+		return length;
+	}
+
+	/**
+	 * Method to calculate rectangle area
+	 * 
+	 * @return the area
+	 */
+	public double getArea() {
 		return (width * length);
 	}
-	
+
 	/**
-	 * 
+	 * toString method to format output display
 	 */
 	@Override
-	public String toString(){
-		return String.format("Rectangle width = %f\nRectangle length = %f\nRectangle area = %f",
-				width, length, getArea());
+	public String toString() {
+		return String.format("Rectangle width = %f\nRectangle length = %f\nRectangle area = %f", width, length,
+				getArea());
 	}
 
 }
